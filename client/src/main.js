29dd741740b318c8b6d5b2e41cc149748418d877
hexatester <revolusi147id@gmail.com>
2020-07-25 00:09:54 +0700

    install vue-socket.io

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,13 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import Vue from 'vue'
 import App from './App.vue'
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from 'socket.io-client'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+const host = process.env.HOST | 'localhost'
+const port = host === 'localhost' ? ':3000' : ''
+const io = SocketIO(`http://${host}${port}`)
 
 Vue.config.productionTip = false
 
@@ -11,6 +17,13 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+// Socket
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: io
+  })
+)
 
 new Vue({
   render: h => h(App)
